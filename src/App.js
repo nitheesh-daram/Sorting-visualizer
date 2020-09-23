dc1a90855d5eb3e88899c5b83c091e1b5cf2d9ed
nitheesh daram <daramnitheesh@gmail.com>
2020-09-23 17:45:15 +0530

    added skeleton pdf and created divs

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Bar from "./components/Bar/Bar";
 function App() {
   return (
     <div className="App">
-       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/">Sorting Visualizer</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">
+          Sorting Visualizer
+        </a>
+      </nav>
+      <section className="whole_container">
+        <div className="simulator">
+          <Bar/>
+          <Bar/>
+        </div>
+        <div className="controls">{/* creating controls */}</div>
+      </section>
     </div>
   );
 }
---
+++ src/components/Bar/Bar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Bar.css";
+
+export const Bar = () => {
+  return (
+    <div className="bar">
+    </div>
+  );
+};
+
+export default Bar;
