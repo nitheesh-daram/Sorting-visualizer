7ad20669132e471e307bd423c494a8856daa4d63
nitheesh daram <daramnitheesh@gmail.com>
2020-09-24 19:19:53 +0530

    added comparisions and swaps meter

--- src/components/Simulator/Simulator.js
+++ src/components/Simulator/Simulator.js
@@ -11,6 +11,8 @@ export default class Simulator extends Component {
     super();
     this.state = {
       array: [],
+      comparisions: 0,
+      swaps: 0,
     };
   }
 
@@ -20,12 +22,14 @@ export default class Simulator extends Component {
 
   resetarray() {
     const array = [];
+    const comparisions = 0;
+    const swaps = 0;
     // Array size
     let size = document.getElementById("size").value;
     for (let index = 0; index < size; index++) {
       array.push(randomIntfromRange(10, 500));
     }
-    this.setState({ array });
+    this.setState({ array, comparisions, swaps });
   }
 
   animate(animations) {
@@ -34,13 +38,21 @@ export default class Simulator extends Component {
       let comparison = animations[index].comparision;
       let swap = animations[index].swap;
       setTimeout(() => {
-        arrayBars[comparison[1]].style.backgroundColor = "red";
-        arrayBars[comparison[0]].style.backgroundColor = "red";
+        arrayBars[comparison[1]].style.backgroundColor = "black";
+        this.state.comparisions = this.state.comparisions + 1;
+        arrayBars[comparison[0]].style.backgroundColor = "black";
+        let comp = document.getElementById("comp");
+        comp.innerHTML = "Comparisions : " + this.state.comparisions;
       }, index * speed);
       setTimeout(() => {
         let temp = arrayBars[swap[1]].style.height;
         arrayBars[swap[1]].style.height = arrayBars[swap[0]].style.height;
         arrayBars[swap[0]].style.height = temp;
+        if (swap[0] !== swap[1]) {
+          this.state.swaps = this.state.swaps + 1;
+          let comp = document.getElementById("swap");
+          comp.innerHTML = "Swaps : " + this.state.swaps;
+        }
       }, index * speed);
       setTimeout(() => {
         const arrayBars = document.getElementsByClassName("array-bar");
@@ -86,10 +98,13 @@ export default class Simulator extends Component {
           <input
             id="size"
             type="range"
-            min="10"
-            max="150"
+            min="150"
+            max="450"
             onChange={() => this.resetarray()}
           ></input>
+
+          <h6 id="comp">Comparisions : {this.state.comparisions}</h6>
+          <h6 id="swap">Swaps : {this.state.swaps}</h6>
         </div>
       </>
     );
@@ -97,7 +112,7 @@ export default class Simulator extends Component {
 }
 
 function randomIntfromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 function arraysareequal(jssorted, algosorted) {
