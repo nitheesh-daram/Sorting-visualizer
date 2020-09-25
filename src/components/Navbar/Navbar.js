782f7484292bb1f8b38d846a49a67b562801af78
nitheesh daram <daramnitheesh@gmail.com>
2020-09-25 17:59:40 +0530

    added credits and some minor optimisations

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,11 +2,20 @@ import React from "react";
 
 const Navbar = () => {
   return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">
-          Sorting Visualizer
-        </a>
-      </nav>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="/">
+        Sorting Visualizer
+      </a>
+        <ul class="navbar-nav">
+          <li className="nav-item active" style={{color:"white",fontSize:"80%",marginTop:"10%"}}>
+            - Made with{" "}
+            <span role="img" aria-label="">
+              ❤️
+            </span>{" "}
+            by Nitheesh Daram
+          </li>
+        </ul>
+    </nav>
   );
 };
 
