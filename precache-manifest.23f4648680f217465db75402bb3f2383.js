033ef8c796bc1ae6a23391802a7ff44d0164431d
nitheesh daram <daramnitheesh@gmail.com>
2020-09-27 14:29:46 +0530

    Updates

---
+++ precache-manifest.23f4648680f217465db75402bb3f2383.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df5923b33e50c15eb2a63f922292984",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82762db22199637abb9a",
+    "url": "/static/css/main.5f88d2db.chunk.css"
+  },
+  {
+    "revision": "cde9c843322d486c0bc2",
+    "url": "/static/js/2.16ee61c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16ee61c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82762db22199637abb9a",
+    "url": "/static/js/main.6f4fa69a.chunk.js"
+  },
+  {
+    "revision": "72c07dcb21740fe3cff4",
+    "url": "/static/js/runtime-main.b593c7cf.js"
+  },
+  {
+    "revision": "742432a18af27bb34482e316a9a6f11a",
+    "url": "/static/media/lighting.742432a1.svg"
+  }
+]);
