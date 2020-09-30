067e0a8784deaa2e704ccbc9a028aae1238f14df
nitheesh daram <daramnitheesh@gmail.com>
2020-09-30 21:34:01 +0530

    added github-corner

--- src/Sorting/Sorting.js
+++ src/Sorting/Sorting.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Simulator from "../components/Simulator/Simulator";
-import Github from "../components/Github/Github";
+import GithubCorner from "react-github-corner";
 const Sorting = () => {
   return (
     <div>
       <Navbar />
-      <Github/>
+      <GithubCorner
+        href="https://github.com/nitheesh-daram/Sorting-visualizer"
+        bannerColor="#fff"
+        octoColor="#000"
+        size={80}
+        direction="right"
+      />
       <section className="whole_container">
         <Simulator />
       </section>
