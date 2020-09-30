333be31dbdf2534b1bd072dcff90656b345d476f
nitheesh daram <daramnitheesh@gmail.com>
2020-09-30 21:14:43 +0530

    trying to add github logo on corner

--- src/Sorting/Sorting.js
+++ src/Sorting/Sorting.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Simulator from "../components/Simulator/Simulator";
-
+import Github from "../components/Github/Github";
 const Sorting = () => {
   return (
     <div>
       <Navbar />
+      <Github/>
       <section className="whole_container">
         <Simulator />
       </section>
